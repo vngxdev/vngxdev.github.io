9f9276799159b514da509337e8faa07076169b9b
Anggi <anggi@gradin.co.id>
2022-06-12 01:01:37 +0700

    optimize SEO

---
+++ _nuxt/static/1654969871/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1654969871",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e9f0c32a"}}}}(null,"\u002F"));
