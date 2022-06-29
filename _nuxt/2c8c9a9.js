5a044f93bbcad2fc1288395398c36bdfabef9def
Anggi <anggi@gradin.co.id>
2022-06-30 01:35:54 +0700

    rev design

---
+++ _nuxt/2c8c9a9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{316:function(n,t,e){"use strict";e.r(t);var c={asyncData:function(n){n.params},data:function(){return{uid:0}}},r=e(27),component=Object(r.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
---
+++ _nuxt/static/1656527677/toam/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/toam", {data:[{}],fetch:{},mutations:void 0});
