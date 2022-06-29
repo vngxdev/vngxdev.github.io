39082ebd9ad5d8b0b2967a5ee677ee1c81b2ddd4
Anggi <anggi@gradin.co.id>
2022-06-30 01:38:56 +0700

    rev about pt for lg

--- _nuxt/static/1656527677/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656527677",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8b7ab1e7"}}}}(null,"\u002F"));
--- _nuxt/static/1656527677/toam/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656527677",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftoam",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8b7ab1e7"}}}}(null));
